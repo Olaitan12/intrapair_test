c6da78c66ec2b228109447af6720ae6c05545de9
olaitan12 <olaitanaderoju@gmail.com>
2022-05-16 16:21:38 +0100

    first commit

---
+++ lib/core/app_export.dart
@@ -0,0 +1,28 @@
+export 'package:get/get.dart';
+
+export 'package:intrapair_test/localization/app_localization.dart';
+
+export 'package:intrapair_test/core/constants/constants.dart';
+
+export 'package:intrapair_test/core/utils/image_constant.dart';
+export 'package:intrapair_test/core/utils/color_constant.dart';
+
+export 'package:intrapair_test/core/utils/math_utils.dart';
+export 'package:intrapair_test/core/utils/pref_utils.dart';
+
+export 'package:intrapair_test/core/utils/initial_bindings.dart';
+
+export 'package:intrapair_test/theme/app_style.dart';
+export 'package:intrapair_test/theme/app_decoration.dart';
+
+export 'package:connectivity_plus/connectivity_plus.dart';
+
+export 'package:intrapair_test/routes/app_routes.dart';
+
+export 'package:intrapair_test/data/models/selectionPopupModel/selection_popup_model.dart';
+
+export 'package:intrapair_test/core/widgets/common_network_image.dart';
+export 'package:intrapair_test/core/widgets/paginated_list_view.dart';
+
+export 'package:intrapair_test/core/errors/exceptions.dart';
+export 'package:intrapair_test/core/network/network_info.dart';
---
+++ lib/data/apiClient/api_client.dart
@@ -0,0 +1,3 @@
+import 'package:intrapair_test/core/app_export.dart';
+
+class ApiClient extends GetConnect {}
